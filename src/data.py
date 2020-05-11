484daf7d86c5d44e465b8fcf0582606ee88663e4
Simon Worthington <simon.worthington@digital.trade.gov.uk>
2020-05-11 11:22:45 +0100

    WIP include trade remedies, suspensions and CN10s

--- src/data.py
+++ src/data.py
@@ -5,7 +5,8 @@ import xlrd
 
 
 ERROR_CTYPE = 5
-
+TRADE_REMEDY_FOOTNOTE = '₁'
+SUSPENSION_FOOTNOTE = '₂'
 
 def excel_to_json(input_file: str, output_file: str, sheet_name: str = "Sheet1"):
     workbook = xlrd.open_workbook(input_file)
@@ -14,16 +15,32 @@ def excel_to_json(input_file: str, output_file: str, sheet_name: str = "Sheet1")
     data = []
 
     for row_number, row in enumerate(worksheet.get_rows()):
-        if any(item.ctype == ERROR_CTYPE for item in row):
-            print(f"Failed row with code {row[0].value}")
+        if row_number == 0:
             continue
+
+        if any(item.ctype == ERROR_CTYPE for item in row):
+            print(f"Failed row with code {row[1].value}")
+            continue
+
+        if (type(row[1].value) is not str):
+            print(f"Row {row_number}: CC is not a string, is {type(row[1].value)}")
+            continue
+
+        if ((row[0].value == 'CN8' and len(row[1].value) != 8) or
+            (row[0].value == 'CN10' and len(row[1].value) != 10) or
+            not (row[0].value == 'CN10' or row[0].value == 'CN8')):
+           print(f"Row {row_number}: code '{row[1].value}' has incorrect CC length")
+           continue
+
         data.append(
             {
-                "commodity": row[0].value,
+                "commodity": row[1].value,
                 "description": row[2].value,
                 "cet_duty_rate": row[3].value,
                 "ukgt_duty_rate": row[4].value,
-                "change": row[5].value,
+                "change": row[5].value.replace(TRADE_REMEDY_FOOTNOTE, '').replace(SUSPENSION_FOOTNOTE, ''),
+                "trade_remedy": (TRADE_REMEDY_FOOTNOTE in row[5].value),
+                "suspension": (SUSPENSION_FOOTNOTE in row[5].value)
             }
         )
 
